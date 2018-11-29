ebe9a227b907a61384ade80e0dceea555db2801a
subdown <subdown@gmx.net>
2018-11-29 22:45:02 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'db' => 
+  array (
+    'host' => 'localhost',
+    'port' => '3306',
+    'username' => 'd02c92d9',
+    'password' => '5Qpfqh39NpYPGwRCK7Vm',
+    'dbname' => 'd02c92d9',
+  ),
+);
